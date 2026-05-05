044debf61ce3142058e7cda1ac94f649b335a539
anass <anas.shaban.awaad@gmail.com>
2026-05-05 23:31:25 +0300

    feat: fix problem of swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flasgger import Swagger
 
 from app.config.cloudinary_config import init_cloudinary
 from app.config.settings import Settings
 from app.controllers.enhance_controller import enhance_bp
 from app.extensions import cors
-from flasgger import Swagger
 
-swagger = Swagger(template=None)
-
+swagger = Swagger()
 
 def create_app():
     app = Flask(__name__)
@@ -18,8 +17,9 @@ def create_app():
     settings = Settings.from_env()
     settings.validate_cloudinary()
 
-    app.config["SWAGGER"] = settings.swagger_config()
+    # ✅ الحل هنا
     swagger.template = settings.swagger_template()
+    swagger.config = settings.swagger_config()
     swagger.init_app(app)
 
     if settings.enable_cors:
